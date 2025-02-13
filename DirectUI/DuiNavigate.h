a65c7b080e2b87911a226ad4bd54cae36c58c685
Olive <oliveistyping@gmail.com>
2025-02-12 17:48:02 -0700

    Forgot a constructor

--- DirectUI/DuiNavigate.h
+++ DirectUI/DuiNavigate.h
@@ -5,6 +5,8 @@ namespace DirectUI
 	class DuiNavigate
 	{
 	public:
+		DuiNavigate& operator=(DuiNavigate const&);
+
 		UILIB_API static Element* WINAPI Navigate(Element* peFrom, DynamicArray<Element*>* pelConsider, int nNavDir);
 	};
 }
